1f61714da5cdd4e443bd831e24b5d32100245f38
Rupnesh <sharmarupnesh@gmail.com>
2021-10-27 11:04:57 +0530

    Initial Commit TODO

--- src/components/ToDo.vue
+++ src/components/ToDo.vue
@@ -18,7 +18,7 @@
       <thead>
         <tr>
           <th scope="col" style="width: 100px">ID</th>
-          <th scope="col">Task</th>
+          <th scope="col">Task<span class="fa fa-pen" style="margin-left: 12px"></span></th>
           <th scope="col" style="width: 180px">Status</th>
           <th scope="col" style="width: 180px">Action</th>
         </tr>
@@ -26,7 +26,14 @@
       <tbody>
         <tr v-for="(task, index) in tasks" :key="index">
           <th scope="row" style="width: 100px">{{index+1}}</th>
-          <td><span :class="{'finished': task.status ==='finished'}">{{task.task}}</span></td>
+          <td>
+            <span  v-show = "task.edit === false" :class="{'finished': task.status ==='finished'}" @dblclick="task.edit = true" >{{task.task}}</span>
+            <!--<input v-else v-model="tasks[index].task" type="text" placeholder="Enter task name" class="form-control" v-on:keyup.enter="onEnter"/>
+          -->
+          <input v-show="task.edit == true" v-model = "task.task" v-on:blur= "task.edit=false;" 
+              @keyup.enter="task.edit=false; update(index,task)">
+          
+          </td>
           <td style="width: 180px" class="text-capitalize" role="button"><span @click="changeStatus(index)" :class="{'text-danger': task.status ==='to-do', 'text-warning': task.status ==='in-progress','text-success': task.status ==='finished'}">{{task.status}}</span></td>
           <td class="d-flex" style="width: 180px">
             <div style="flex:1" @click="editTask(index)" role="button"><span class="fa fa-pen"></span></div>
@@ -35,7 +42,6 @@
         </tr>
       </tbody>
     </table>
-
   </div>
 </template>
 
@@ -45,44 +51,59 @@ export default {
   props: {
     msg: String
   },
+  mounted() {
+    this.countTask();
+  },
   data() {
     return {
       task: '',
       editedTask: null,
+      editTaskText: false,
       availabelStatus: ['to-do', 'in-progress','finished'],
       tasks: [ 
-        {task: "task 1", status: "to-do"},
-        {task: "task 2", status: "to-do"},
-        {task: "task 3", status: "in-progress"},
-        {task: "task 4", status: "to-do"},
-        {task: "task 5", status: "finished"},
+        {task: "task 1", status: "to-do", edit: false},
+        {task: "task 2", status: "to-do", edit: false},
+        {task: "task 3", status: "in-progress", edit: false},
+        {task: "task 4", status: "to-do", edit: false},
+        {task: "task 5", status: "finished", edit: false},
       ]
     }
   },
 
   methods : {
+    
     submitTask() {
       if(this.task.length === 0 ) return;
       if(this.editedTask === null)
-        this.tasks.push({task: this.task, status: "to-do"})
+        this.tasks.push({task: this.task, status: "to-do", edit: false})
       else {
         this.tasks[this.editedTask].task = this.task;
         this.tasks[this.editedTask].status = this.availabelStatus[0];
         this.editedTask = null;
       }
-      this.task = ''
+      this.task = '';
+      this.countTask();
     },
     deleteTask(index) {
-      this.tasks.splice(index,1)
+      this.$dialog
+      .confirm('Please confirm to continue')
+      .then(dialog=> {
+        console.log('Clicked on proceed',dialog);
+        this.tasks.splice(index,1)
+      })
+      .catch(()=> {
+        console.log('Clicked on cancel');
+      });
+      
     },
     editTask(index) {
       this.task = this.tasks[index].task;
       this.editedTask = index;
+      this.editTaskText = true;
     },
     changeStatus(index) {
-      let newIndex = this.availabelStatus.indexOf(this.tasks[index].status);
-      if(++newIndex > 2) newIndex = 0;
-      this.tasks[index].status = this.availabelStatus[newIndex];
+      const newIndex = this.availabelStatus.indexOf(this.tasks[index].status) + 1;
+      this.tasks[index].status = this.availabelStatus[newIndex > 2 ? newIndex = 0 : newIndex];
     },
     onEnter() {
       this.submitTask();
@@ -91,10 +112,13 @@ export default {
       this.$emit('showTasksCount', this.tasks.length);
     },
     countSpecificTask(task) {
-      var result = this.tasks.reduce((previousValue, currentObject) => {
+      return this.tasks.reduce((previousValue, currentObject) => {
           return previousValue + (currentObject.status === task); 
       }, 0);
-      return result;
+      
+    },
+    update(index,data) {
+      this.tasks[index].task = data.task;
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -8,6 +8,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 
+import VuejsDialog from 'vuejs-dialog';
+ 
+// include the default style
+import 'vuejs-dialog/dist/vuejs-dialog.min.css';
+ 
+// Tell Vue to install the plugin.
+Vue.use(VuejsDialog);
+
 
 Vue.use(BootstrapVue);
 
